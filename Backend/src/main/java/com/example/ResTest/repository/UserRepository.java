27a0b6d1149bc5e0322e31c442cb48f0bf98e4ff
douglasmasuzzo <dev.masdouglas4001@gmail.com>
2026-05-11 00:20:29 -0300

    feat (repository): adiciona repositórios JPA para User, MockEndpoint e RequestLog

---
+++ Backend/src/main/java/com/example/ResTest/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.ResTest.repository;
+
+
+import com.example.ResTest.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
