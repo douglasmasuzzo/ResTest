f75f198982afd2d692cd10329eb515604e4df39a
douglasmasuzzo <dev.masdouglas4001@gmail.com>
2026-05-11 00:41:24 -0300

    test (app): adiciona o teste de contexto gerado pelo Spring Initializer

---
+++ Backend/src/test/java/com/example/ResTest/ResTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ResTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
