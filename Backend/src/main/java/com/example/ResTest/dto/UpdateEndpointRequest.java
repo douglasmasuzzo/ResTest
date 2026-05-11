b60b415102c62b5eaa759585b6e84877d18fa767
douglasmasuzzo <dev.masdouglas4001@gmail.com>
2026-05-11 00:21:35 -0300

    feat (dto): adiciona DTO's de request e response

---
+++ Backend/src/main/java/com/example/ResTest/dto/UpdateEndpointRequest.java
@@ -0,0 +1,8 @@
+package com.example.ResTest.dto;
+
+public class UpdateEndpointRequest {
+    public String payload;
+    public Integer statusCode;
+    public Integer delayMs;
+    public String label;
+}
