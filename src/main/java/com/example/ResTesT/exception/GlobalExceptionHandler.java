9a052eeb0ec29f0a2c693d1161d5f819235f41a1
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2026-05-28 00:51:11 -0300

    Criando exceções Runtime, status code 400 e 500

---
+++ src/main/java/com/example/ResTesT/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.ResTesT.exception;
+
+// Importa a classe ResponseEntity para respostas HTTP
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Define esta classe como um tratador global de exceções da aplicação
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Trata exceções do tipo CustomException
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustom(CustomException ex) {
+
+        // Retorna status 400 (Bad Request)
+        // junto com a mensagem da exceção
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+
+    // Trata qualquer exceção genérica não tratada anteriormente
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneric(Exception ex) {
+
+
+        // Exibe o erro completo no console da aplicação
+        ex.printStackTrace();
+
+        // Retorna status 500 (Internal Server Error)
+        // com mensagem genérica
+        return ResponseEntity.internalServerError().body("Erro interno");
+    }
+}
